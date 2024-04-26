d86615103051336c31d9c40342c9f4038ec3e777
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-27 04:31:28 +0700

    refactor character counting function to return a list of dictionaries

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     file_contents = get_book_text(file_path)
 
     print(get_num_words(file_contents))
-    print(get_char_dict(file_contents))
+    print(get_char_list(file_contents))
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -13,14 +13,28 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_char_dict(text):
-    characters = {}
+def get_char_list(text):
+    char_dict = {}
     for char in text:
         lowered = char.lower()
-        if lowered in characters:
-            characters[lowered] += 1
+        if lowered in char_dict:
+            char_dict[lowered] += 1
         else:
-            characters[lowered] = 1
-    return characters
+            char_dict[lowered] = 1
+
+    character_list = []
+    for char in char_dict:
+        character_list.append({
+            "char": char,
+            "num": char_dict[char]
+        })
+    return character_list
+
+def print_report(text, path):
+    print(f"--- Report of {path} ---")
+    print(f"{get_num_words(text)} words found in the document")
+    print()
+
+
 
 main()
