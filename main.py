3296f64455c5959e58fbcfb4977b512d481b88a0
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-27 03:57:27 +0700

    refactor file reading

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 def main():
     file_path = "books/frankenstein.txt"
-    with open(file_path) as f:
-        file_contents = f.read()
+    file_contents = get_book_text(file_path)
 
     print(count_words(file_contents))
+
+def get_book_text(file_path):
+    with open(file_path) as f:
+        return f.read()
 
 def count_words(text):
     words = text.split()
