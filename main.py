4f47fdb5343e4f0d43f5899cf5b05f1345e5629f
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-27 04:24:09 +0700

    rename word-count and char-count functions to be more descriptive

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ def main():
     file_path = "books/frankenstein.txt"
     file_contents = get_book_text(file_path)
 
-    print(count_words(file_contents))
-    print(count_characters(file_contents))
+    print(get_num_words(file_contents))
+    print(get_char_dict(file_contents))
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
-def count_words(text):
+def get_num_words(text):
     words = text.split()
     return len(words)
 
-def count_characters(text):
+def get_char_dict(text):
     characters = {}
     for char in text:
         lowered = char.lower()
