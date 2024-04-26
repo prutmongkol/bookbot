b63aedeb547d6a713b70db0818f1d131efff0649
elfaron <150164864+PrutMongkol@users.noreply.github.com>
2024-04-27 04:17:34 +0700

    add character counting function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     file_contents = get_book_text(file_path)
 
     print(count_words(file_contents))
+    print(count_characters(file_contents))
 
 def get_book_text(file_path):
     with open(file_path) as f:
@@ -10,6 +11,16 @@ def get_book_text(file_path):
 
 def count_words(text):
     words = text.split()
-    return len(words)    
+    return len(words)
+
+def count_characters(text):
+    characters = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in characters:
+            characters[lowered] += 1
+        else:
+            characters[lowered] = 1
+    return characters
 
 main()
